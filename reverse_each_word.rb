339cdc510fa3c1b0ca020ca042dd42eb823d6268
kovner chalumo <kchalumo@gmail.com>
2018-11-27 22:05:05 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(arr)
   rarr = []
   arr.split.each do |word|
-    puts word.reverse 
-    rarr << word.reverse
+    rev = word.reverse 
+    rarr << rev
   end.join(' ')
 end
   
